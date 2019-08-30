28055dbf55d9e541f77731d46817a477a0469247
harinda05 <harindaburndown@gmail.com>
2019-08-30 10:25:52 +0530

    initial commit

---
+++ src/main/java/JavaDatabase/MysqlDBConnection/DBconnection.java
@@ -0,0 +1,25 @@
+package JavaDatabase.MysqlDBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+	private static String dbhost = "jdbc:mysql://localhost:3306/student";
+	private static String username = "root";
+	private static String password = "1234";
+	private static Connection conn;
+	
+	@SuppressWarnings("finally")
+	public static Connection createNewDBconnection() {
+		try  {	
+			conn = DriverManager.getConnection(
+					dbhost, username, password);	
+		} catch (SQLException e) {
+			System.out.println("Cannot create database connection");
+			e.printStackTrace();
+		} finally {
+			return conn;	
+		}		
+	}
+}
---
+++ src/main/java/JavaDatabase/MysqlDBConnection/Student.java
@@ -0,0 +1,49 @@
+package JavaDatabase.MysqlDBConnection;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private String address;
+	private String course_code;
+	
+	
+	// return id
+	public int getId() {
+		return id;
+	}	
+	// set id
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	// return name
+	public String getName() {
+		return name;
+	}	
+	// set name
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+	
+	// return address
+	public String getAddress() {
+		return address;
+	}	
+	// set address
+	public void setAddress(String address) {
+		this.address = address;
+	}
+		
+	
+	// return course_code
+	public String getCourse_code() {
+		return course_code;
+	}	
+	// set course_code
+	public void setCourse_code(String course_code) {
+		this.course_code = course_code;
+	}
+}
